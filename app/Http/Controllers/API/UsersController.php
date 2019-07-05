2a8338c1d769ae6838d418019a577efef2c8038a
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-07-05 15:23:57 +0300

    back: get all parts fixing

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
-use App\Model\User;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
