6fd95e03e40a936d97a2530cc47574440e1f74c8
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-07-05 15:03:27 +0300

    back: get all parts edited

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\User;
 use Illuminate\Support\Facades\Auth;
-
+use Validator;
 
 class UsersController extends Controller
 {
