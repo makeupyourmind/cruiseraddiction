ceb16b78e21923553c3798aacefa0bac09f2bcde
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-07-22 11:54:21 +0300

    add stock_ca route

--- database/migrations/2019_07_22_074322_add_column_for_table_parts.php
+++ database/migrations/2019_07_22_074322_add_column_for_table_parts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class AddColumnForTableParts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        Schema::table('parts', function (Blueprint $table) {
+	    $table->boolean('is_stock_ca')->default(false);
+	});
     }
 
     /**
@@ -27,6 +25,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+	//
     }
 }
