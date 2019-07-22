b59579fab261bbd85e3f27bee7c68f9a5472f0fd
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-07-22 12:28:04 +0300

    add field to parts

--- app/Model/Part.php
+++ app/Model/Part.php
@@ -10,7 +10,8 @@ class Part extends Model
 
     protected $fillable = [
         'part_number', 'description_english', 'weight_physical', 'weight_volumetric', 'brand_name', 'description_full',
-        'color', 'is_bundle', 'subst_for', 'qty', 'price', 'part_fits', 'image', 'warehouse', 'modified_by', 'notes', 'unique_hash', 'is_stock_ca'
+        'color', 'is_bundle', 'subst_for', 'qty', 'price', 'part_fits', 'image', 'warehouse', 'modified_by', 'notes',
+        'unique_hash', 'is_stock_ca','categories', 'tags', 'min_price', 'max_price', 'min_stock'
     ];
 
 
