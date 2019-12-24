83ef7e776a0009730092be73240ac9d727d59d8e
makeupyourmind <nikitosnov@gmail.com>
2019-12-24 22:55:24 +0200

    test pdf generate

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -5,28 +5,29 @@ use App\Model\Order;
 use Excel;
 use App\Exports\OrdersExport;
 use Mail;
-
+use PDF;
+use Illuminate\Support\Str;
+use Storage;
 use Illuminate\Http\Request;
 
 class TestController
 {
     public function test(){
-        // echo 'test controller';
-        date_default_timezone_set('Canada/Eastern');
-        echo "Order parser is started : ".date('Y/m/d H:i:s')."\n";
-        $orders = Order::with(['user'])->where('isCheckedParser', 0)->get();
-        $pathToFile = storage_path('app/orders.xls');
-        if(count($orders) > 0){
-            $store = Excel::store(new OrdersExport(), 'orders.xls', 'local');
-            Mail::send([], [] ,function($message) use ($pathToFile) {
-                $message->to('nikitosnov@gmail.com') //order@vivat-uae.net
-                        ->subject('Orders');           
-                $message->attach($pathToFile);
-            });           
-            return 'Order parse done. Successfully';
-        }
-        else{
-            return 'Order parse done.';
-        }
+        $user_email = 'your_email';
+        $pass_data_to_pdf = [
+            'order_id' => '',
+        ];
+        $pdf = PDF::loadView('payment_file_history/index', $pass_data_to_pdf);
+        $pdf->setOptions(['isPhpEnabled' => true, "enable_php" => true]);
+        $content_pdf = $pdf->download()->getOriginalContent();
+        $unique_hash_string = Str::random(22);
+        Storage::disk('public_uploads')->put("payment_file_history/{$unique_hash_string}.pdf", $content_pdf);
+        $pathToFile = Storage::disk('public_uploads')->path("payment_file_history/{$unique_hash_string}.pdf");
+        Mail::send('email.payment_done', [''], function ($message) use ($user_email, $pathToFile) {
+            $message->to($user_email)
+                    ->subject('Thank you for your business!')
+                    ->attach($pathToFile);
+        });
+       return 'okay';
     }
 }
