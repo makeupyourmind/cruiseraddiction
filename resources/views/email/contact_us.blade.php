f71713f25ae0d914e148a75225704a73829c1569
igor_smirnoff_zp <smirnoffnew@gmail.com>
2020-01-11 15:51:36 +0200

    add text for email

--- resources/views/email/contact_us.blade.php
+++ resources/views/email/contact_us.blade.php
@@ -17,42 +17,42 @@
     <br>
     @foreach ($variables as $key => $value)
         @if($key == 'name')
-            <p style="width: 200px">
+            <div style="width: 200px">
                 <div style="float: left; font-weight: bold">Full Name:</div>
                 <div style="float: right">{{ $value }}</div>
-            </p>
+            </div>
             <div style="clear: both"></div>
             <br>
         @endif
         @if($key == 'email')
 
-            <p style="width: 200px">
+            <div style="width: 200px">
                 <div style="float: left; font-weight: bold">Email Address:</div>
                 <div style="float: right">{{ $value }}</div>
-            </p>
+            </div>
             <div style="clear: both"></div>
             <br>
         @endif
         @if($key == 'phone')
 
-            <p style="width: 200px">
+            <div style="width: 200px">
                 <div style="float: left; font-weight: bold">Phone Number:</div>
                 <div style="float: right">{{ $value }}</div>
-            </p>
+            </div>
             <div style="clear: both"></div>
             <br>
         @endif
         @if($key == 'message')
-            <p style="width: 200px">
+            <div style="width: 200px">
                 <div style="float: left; font-weight: bold">Order Number:</div>
-            </p>
+            </div>
             <div style="clear: both"></div>
             <div style="float: right">{{ $value }}</div>
             <br>
         @endif
     @endforeach
     <h3>CRUISER ADDICTION - TOYOTA OEM LANDCRUISER 4X4 PARTS </h3>
-    <a>http://cruiseraddiction.com/</a>
+    <a href="http://cruiseraddiction.com">http://cruiseraddiction.com/</a>
     <hr>
     CRUISER ADDICTION TOYOTA OEM LANDCRUISER 4X4 PARTS is powered by BigCommerce.  Launch your own store for free with BigCommerce.
 </body>
