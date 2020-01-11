e46fce2afa2c1765de27dfc567032aba5514eca0
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2020-01-11 08:26:46 -0500

    parser

--- parser/config.js
+++ parser/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    DB_USERNAME: 'root',
-    DB_PASSWORD: 'root',
-    DB_DATABASE: 'cruiser',
-    DB_HOST: 'localhost',
+    DB_USERNAME: 'testback_db',
+    DB_PASSWORD: 'Ukraine2020db',
+    DB_DATABASE: 'testback_db',
+    DB_HOST: '192.168.0.144',
     PROXY_URL: '217.12.49.197:1234',
     AMAYAMA: 'https://www.amayama.com/en',
     TOYOTA_PARTS_DEAL: 'https://www.toyotapartsdeal.com/',
