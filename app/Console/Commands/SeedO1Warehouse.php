e00aa395ff8653077650b344dc584c18241eb3dc
makeupyourmind <nikitosnov@gmail.com>
2020-04-12 20:20:14 +0300

    fix

--- app/Console/Commands/SeedO1Warehouse.php
+++ app/Console/Commands/SeedO1Warehouse.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Model\E1_WH;
 use App\Model\PartNumberInfo;
 use App\Model\O1_WH;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 
 class SeedO1Warehouse extends Command
@@ -14,7 +16,7 @@ class SeedO1Warehouse extends Command
      *
      * @var string
      */
-    protected $signature = 'seed:o1';
+    protected $signature = 'seed:o1ande1';
 
     /**
      * The console command description.
@@ -44,7 +46,9 @@ class SeedO1Warehouse extends Command
 
         [$o1] = Excel::toCollection(null, 'o1_wh_SEED.csv', 'local')->toArray();
         echo "Got usa parts : " . date('Y/m/d H:i:s') . "\n";
-
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        O1_WH::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $counter = 0;
         if (count($o1) > 0) {
             foreach ($o1 as $part) {
@@ -52,12 +56,21 @@ class SeedO1Warehouse extends Command
                     $record = PartNumberInfo::where('raw_part_number', trim(str_replace('-', '', $part[1])))->first();
                     if ($record) {
                         $list_usd = (float) (((((int) $record->a_weight * 6.0) + $part[3] * 0.061 + $part[3]) * 1.3) * 1.037);
-                        O1_WH::create([
-                            'part_number_info_id' => $record->id,
-                            'cost_usd' => $part[7],
-                            'qty' => 1,
-                            'list_usd' => round($list_usd, 2)
-                        ]);
+                        if ($part[6] === "E 1 d.") {
+                            O1_WH::create([
+                                'part_number_info_id' => $record->id,
+                                'cost_usd' => $part[7],
+                                'qty' => $part[5],
+                                'list_usd' => round($list_usd, 2)
+                            ]);
+                        } else {
+                            E1_WH::create([
+                                'part_number_info_id' => $record->id,
+                                'cost_usd' => $part[7],
+                                'qty' => $part[5],
+                                'list_usd' => round($list_usd, 2)
+                            ]);
+                        }
                         $counter++;
                         var_dump($counter);
                     }
